9e40e7e3e310cf7102ffb74795463d1cc6972476
SaraDonzellini <saradonzie@gmail.com>
2023-02-07 14:12:36 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Laravel Primi Passi</title>
+</head>
+<body>
+  <header>
+    <a href="/secondpage">Seconda pagina</a>
+  </header>
+  <main>
+    <h1>
+      I nostri primi passi in Laravel!!
+
+    </h1>
+  </main>
+</body>
+</html>
